287a7a6bc0e1877688975b79021aec9c8c119861
lexvo <157216914+lexvo@users.noreply.github.com>
2025-11-30 20:54:22 +0300

    001

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,14 @@
-import "../pages/index.css"; // импорт главного файла стилей
-
-// import '../styles/index.css';
+import "../styles/index.css"; // импорт главного файла стилей
 
 
 
-// const numbers = [2, 3, 5];
 
-// // Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-// const doubledNumbers = numbers.map(number => number * 2);
 
-// console.log(doubledNumbers); // 4, 6, 10
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
+
+console.log('Hello');
